59a5a3be6c7136b07213c29e05a6b326d9a87ff2
calvinwycheng <calvinwycheng@gmail.com>
2026-08-28 16:55:55 +0800

    Correct requested exam answers and labels

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "azure-training-v30";
-const CORE = ["./", "./index.html", "./styles.css?v=20", "./app.js?v=25", "./db.js?v=6", "./data/questions.js?v=17", "./data/dp600-questions.js?v=7", "./data/courses.js?v=2", "./assets/ai103-logo.png"];
+const CACHE = "azure-training-v31";
+const CORE = ["./", "./index.html", "./styles.css?v=20", "./app.js?v=25", "./db.js?v=6", "./data/questions.js?v=18", "./data/dp600-questions.js?v=8", "./data/courses.js?v=2", "./assets/ai103-logo.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(
   caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())
